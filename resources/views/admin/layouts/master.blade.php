73102fcd771199b71310c68486db8c965132452d
khanhtungmtp <umlatao97@gmail.com>
2019-05-31 11:29:31 +0700

    Cắt giao diện trang quản trị

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>@yield('title')</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="assets/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/admin/css/font_nunito.css"
+          rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+<!-- Page Wrapper -->
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    @include('admin.layouts.menu')
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+
+            <!-- Topbar -->
+            @include('admin.layouts.header')
+            <!-- End of Topbar -->
+
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+
+                @yield('content')
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        @include('admin.layouts.footer')
+
+</body>
+
+</html>
---
+++ resources/views/admin/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layouts.master')
+@section('title')
+    Trang chủ
+@endsection
+
+@section('content')
+    <h1>Trang chủ</h1>
+@endsection
